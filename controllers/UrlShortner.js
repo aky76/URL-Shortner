fb5fcf9fd834ffbcc9f0a81e2eb19f7e78404886
aky <pagalsanjeev95@gmail.com>
2025-06-26 00:20:49 +0530

    for better responsiveness and functioning

--- controllers/UrlShortner.js
+++ controllers/UrlShortner.js
@@ -1,40 +1,30 @@
-import { Url } from "../models/UrlSchema.js";
+ import { Url } from "../models/UrlSchema.js";
 import { nanoid } from "nanoid";
 
 export const shortUrl = async (req, res) => {
-  try {
-    const longUrl = req.body.longUrl;
-    if (!longUrl) {
-      return res.status(400).render("index.ejs", { shortUrl: null });
-    }
+  const longUrl = req.body.longUrl;
+  const shortCode = nanoid();
 
-    const shortCode = nanoid();
+  const shortUrl = http://localhost:1000/${shortCode};
 
-    const shortUrl = `https://url-shortner-omqm.onrender.com/${shortCode}`;
+  // save to database
+  const newUrl = new Url({ shortCode, longUrl });
+  await newUrl.save();
 
-    const newUrl = new Url({ shortCode, longUrl });
-    await newUrl.save();
+  console.log("short saved = ", newUrl);
 
-    console.log("Short saved:", newUrl);
-    res.render("index.ejs", { shortUrl });
-  } catch (error) {
-    console.error("Shortening error:", error.message);
-    res.status(500).render("index.ejs", { shortUrl: null });
-  }
+  res.render("index.ejs", { shortUrl });
 };
 
 export const getOriginalUrl = async (req, res) => {
-  try {
-    const shortCode = req.params.shortCode;
-    const originalUrl = await Url.findOne({ shortCode });
+  const shortCode = req.params.shortCode;
 
-    if (originalUrl) {
-      res.redirect(originalUrl.longUrl);
-    } else {
-      res.status(404).send("Short URL not found");
-    }
-  } catch (err) {
-    console.error("Redirect error:", err.message);
-    res.status(500).send("Server error");
+  // find on database
+  const originalUrl = await Url.findOne({ shortCode });
+
+  if (originalUrl) {
+    res.redirect(originalUrl.longUrl);
+  } else {
+    res.json({ message: "Invalid shortcode" });
   }
 };
